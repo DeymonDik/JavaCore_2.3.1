1e11598f457b4ff9eb20eb90fb1245a617074f58
DeymonDik <deymon91@gmail.com>
2022-09-14 09:38:35 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,23 @@
 package org.example;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        List<Integer> b = new ArrayList<Integer>();
-        for (int i = 0; i<a; i++) {
-            b.add(in.nextInt());
+        String str = in.nextLine();
+        String[] a = str.split(" ");
+        a = Arrays.stream(a).sorted().toArray(String[]::new);
+
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, a);
+        list.sort(Comparator.comparingInt(String::length));
+        a = list.toArray(new String[0]);
+
+        String sim = "";
+        for(String el:a) {
+            System.out.print(sim + el);
+            sim = " ";
         }
-        Map<Integer,Integer> m = b.stream().collect(Collectors.toMap(c->c,c->1,Integer::sum));
-        Map<Integer, Integer> sortedMap = m.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors
-                        .toMap(Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (e1, e2) -> e1,
-                                LinkedHashMap::new));
-        sortedMap.forEach((k,v)->System.out.println(k + " " + v));
     }
 }
