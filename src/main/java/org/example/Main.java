932ce828b60e9e962d63d100d2ddbd9e13c843c8
DeymonDik <deymon91@gmail.com>
2022-09-14 09:30:04 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        Map<List<Integer>,String> map = new LinkedHashMap<>(a);
+        List<Integer> b = new ArrayList<Integer>();
         for (int i = 0; i<a; i++) {
-            List<Integer> list = new ArrayList<>();
-            list.add(in.nextInt());
-            in.skip(" ");
-            String n2 = in.nextLine();
-            map.put(list,n2);
+            b.add(in.nextInt());
         }
-        Map<String, List<Integer>> sortedMap = map.entrySet()
+        Map<Integer,Integer> m = b.stream().collect(Collectors.toMap(c->c,c->1,Integer::sum));
+        Map<Integer, Integer> sortedMap = m.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue())
+                .sorted(Map.Entry.comparingByKey())
                 .collect(Collectors
-                        .toMap(Map.Entry::getValue,
-                                Map.Entry::getKey,
-                                (e1, e2) -> {e1.add(e2.get(0));return e1;},
+                        .toMap(Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e1,
                                 LinkedHashMap::new));
-        sortedMap.forEach((k,v)-> {
-            System.out.print(k);
-            System.out.print(" ");
-            v.sort(Comparator.naturalOrder());
-            v.forEach(list->{
-                System.out.print(list);
-                if(v.indexOf(list)==v.size()-1)System.out.println();
-                else System.out.print(" ");});
-        });
+        sortedMap.forEach((k,v)->System.out.println(k + " " + v));
     }
 }
